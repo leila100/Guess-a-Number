5e9e5254c657dceafed07d79c6d090574b78e113
Leila <nb.leila10@gmail.com>
2020-02-20 18:13:28 -0600

    Add an image from a url

--- components/screens/GameOver.js
+++ components/screens/GameOver.js
@@ -9,7 +9,14 @@ const GameOver = props => {
     <View style={styles.screen}>
       <TitleText>The game is over!!!!</TitleText>
       <View style={styles.imageContainer}>
-        <Image source={require("../../assets/success.png")} style={styles.image} />
+        {/* <Image source={require("../../assets/success.png")} style={styles.image} /> */}
+        <Image
+          source={{
+            uri:
+              "https://merriam-webster.com/assets/mw/images/article/art-wap-landing-mp-lg/alt-597f43cca08a4-4012-4a67efa4a1926905348cbc133634f7ff@1x.jpg"
+          }}
+          style={styles.image}
+        />
       </View>
       <BodyText>Number of rounds: {props.rounds}</BodyText>
       <BodyText>Number was: {props.userNumber}</BodyText>
@@ -26,7 +33,8 @@ const styles = StyleSheet.create({
   },
   image: {
     width: "100%",
-    height: "100%"
+    height: "100%",
+    resizeMode: "cover"
   },
   imageContainer: {
     height: 300,
