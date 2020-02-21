60217ce9d53895de3aa94a6f1896d69948c548d3
Leila <nb.leila10@gmail.com>
2020-02-21 12:42:09 -0600

    Use the Dimentions API
    In a turnary operation
    in if statement to select diffirent styles beased on width

--- components/screens/Game.js
+++ components/screens/Game.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from "react";
-import { View, StyleSheet, Text, Alert, ScrollView, FlatList } from "react-native";
+import { View, StyleSheet, Text, Alert, FlatList, Dimensions } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
 import NumberContainer from "../NumberContainer";
@@ -60,6 +60,9 @@ const GameScreen = props => {
     setPastGuesses(prevGuesses => [nextGuess.toString(), ...prevGuesses]);
   };
 
+  let listContainerStyle = styles.listContainer;
+  if (Dimensions.get("window").width < 350) listContainerStyle = styles.listContainerBig;
+
   return (
     <View style={styles.screen}>
       <Text style={defaultStyles.titleText}>Opponent's Guess</Text>
@@ -72,7 +75,7 @@ const GameScreen = props => {
           <Ionicons name='md-add' size={24} color='white' />
         </MainButton>
       </Card>
-      <View style={styles.listContainer}>
+      <View style={listContainerStyle}>
         {/* <ScrollView contentContainerStyle={styles.list}>
           {pastGuesses.map((guess, index) => renderListItem(guess, pastGuesses.length - index))}
         </ScrollView> */}
@@ -96,7 +99,7 @@ const styles = StyleSheet.create({
   buttonContainer: {
     flexDirection: "row",
     justifyContent: "space-around",
-    marginTop: 20,
+    marginTop: Dimensions.get("window").height > 600 ? 20 : 5,
     width: 400,
     maxWidth: "90%"
   },
@@ -114,6 +117,10 @@ const styles = StyleSheet.create({
   listContainer: {
     flex: 1,
     width: "60%"
+  },
+  listContainerBig: {
+    flex: 1,
+    width: "80%"
   },
   list: {
     flexGrow: 1,
