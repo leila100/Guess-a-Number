e19662f1d7a06ecd245ced385de442dd62c807f5
Leila <nb.leila10@gmail.com>
2020-02-21 19:39:06 -0600

    Use SaefAreaView in app.js to be sure the view area is always showing

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, SafeAreaView } from "react-native";
 import * as Font from "expo-font";
 import { AppLoading } from "expo";
 
@@ -45,10 +45,10 @@ export default function App() {
     screen = <GameOver rounds={guessRounds} userNumber={userNumber} playAgain={playAgainHandler} />;
 
   return (
-    <View style={styles.screen}>
+    <SafeAreaView style={styles.screen}>
       <Header title='Guess a Number' />
       {screen}
-    </View>
+    </SafeAreaView>
   );
 }
 
