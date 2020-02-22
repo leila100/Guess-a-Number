16c7d284a13b93e01e6dd6588fbbf7fad4aa892a
Leila <nb.leila10@gmail.com>
2020-02-21 19:01:02 -0600

    Change the effect on MainButton component when clicked based on the platform

--- components/MainButton.js
+++ components/MainButton.js
@@ -1,15 +1,19 @@
 import React from "react";
-import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import { StyleSheet, View, Text, TouchableOpacity, TouchableNativeFeedback, Platform } from "react-native";
 
 import Colors from "../constants/colors";
 
 const MainButton = props => {
+  let ButtonComponent = TouchableOpacity;
+  if (Platform.OS === "android" && Platform.Version >= 21) ButtonComponent = TouchableNativeFeedback;
   return (
-    <TouchableOpacity activeOpacity={0.6} onPress={props.onPress}>
-      <View style={{ ...styles.button, ...props.style }}>
-        <Text style={styles.buttonText}>{props.children}</Text>
-      </View>
-    </TouchableOpacity>
+    <View style={styles.buttonContainer}>
+      <ButtonComponent activeOpacity={0.6} onPress={props.onPress}>
+        <View style={{ ...styles.button, ...props.style }}>
+          <Text style={styles.buttonText}>{props.children}</Text>
+        </View>
+      </ButtonComponent>
+    </View>
   );
 };
 
@@ -24,6 +28,10 @@ const styles = StyleSheet.create({
     color: "white",
     fontFamily: "open-sans",
     fontSize: 18
+  },
+  buttonContainer: {
+    borderRadius: 25,
+    overflow: "hidden"
   }
 });
 
