a9190ca6f990d56cc46d1fb3f1abf252e45e974f
Leila <nb.leila10@gmail.com>
2020-02-21 19:27:11 -0600

    Use platform-specific code files

--- components/MainButton.android.js
+++ components/MainButton.android.js
@@ -5,7 +5,7 @@ import Colors from "../constants/colors";
 
 const MainButton = props => {
   let ButtonComponent = TouchableOpacity;
-  if (Platform.OS === "android" && Platform.Version >= 21) ButtonComponent = TouchableNativeFeedback;
+  if (Platform.Version >= 21) ButtonComponent = TouchableNativeFeedback;
   return (
     <View style={styles.buttonContainer}>
       <ButtonComponent activeOpacity={0.6} onPress={props.onPress}>
